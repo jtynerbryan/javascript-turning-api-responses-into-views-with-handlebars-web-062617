d70b2a3d1cf5796797444ce2574b9ade1db0c765
Jesse Tyner-Bryan <jesse@Jesses-MacBook-Air.local>
2017-08-15 10:52:27 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function getRepositories() {
+	const req = new XMLHttpRequest()
+	req.addEventListener("load", showRepositories);
+	req.open("Get", 'https://api.github.com/users/octocat/repos')
+	req.send()
+}
+
+function showRepositories(event, data) {
+	const repos = JSON.parse(this.responseText)
+	const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`
+
+	document.getElementById("repositories").innerHTML = repoList
+}
